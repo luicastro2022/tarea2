b34c079044b3fd7d5882417449a8e7eef66e75b5
vicente132 <vihernandez2022@udec.cl>
2023-10-19 11:35:38 -0300

    limpiar el main
    borar ejemplos de prueba

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main{
     public static void main(String[] Args){
-        Doblon d=new Doblon(1);
-        System.out.println(d.comer());
-        System.out.println(d.getSerie());
+        
     }
 }
