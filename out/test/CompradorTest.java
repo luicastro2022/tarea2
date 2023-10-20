76d4f034cfaf116e377b0bcbfe78fb622fe3afe0
vicente132 <vihernandez2022@udec.cl>
2023-10-20 20:43:35 -0300

    nuevo test

--- out/test/CompradorTest.java
+++ out/test/CompradorTest.java
@@ -3,16 +3,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class CompradorTest {
+    void testNormal(){
+        Moneda m=new Moneda1500();
+        Expendedor e=new Expendedor(1);
+        Comprador c=new Comprador(m,1,e);
 
+        assertAll()
+    }
     @Test
-    void test(){
+    void testSinBebida(){
         Moneda m=new Moneda1500();
         Expendedor e=new Expendedor(0);
         Comprador c=new Comprador(m,1,e);
 
         assertAll(
                 ()->assertEquals(null,c.getSonidob())
-
+                ()->assertEquals();
         );
     }
 }
